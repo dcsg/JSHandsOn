c58d0b6eca48310003834b1155116344838cf7ab
Daniel Gomes <daniel.gomes@reset.pt>
2011-11-17 22:11:14 +0000

    Added sort order on datagrid

--- knockoutGrid/js/rstGrid.js
+++ knockoutGrid/js/rstGrid.js
@@ -3,7 +3,10 @@
  * Date: 14/11/11
  * Time: 01:16
  */
+
 $(document).ready(function(){
+    var gridField;
+
     var myModel = {
         items: ko.observableArray([
             { firstName: "Magda", lastName: "Monteiro", language: "PT"},
@@ -14,13 +17,58 @@ $(document).ready(function(){
             { firstName: "Miguel", lastName: "Vasco", language: "PT" },
             { firstName: "Joaquim", lastName: "Lopes", language: "PT" }
         ])
-        /*,
-        sortByName: function () {
-            this.items.sort(function (a, b) {
-                return a.name < b.name ? -1 : 1;
-            });
-        }*/
+        ,
+        sortByName: function (event, _sortOrder, _gridField)
+        {
+
+
+            gridField = _gridField;
+
+            var replace;
+
+            if(_sortOrder == "ASC")
+            {
+                this.items.sort(sortASC);
+                var label = _gridField.replace("Name","");
+                replace = $(event.currentTarget).attr("data-bind");
+                replace = replace.replace("ASC", "DESC");
+                var field = $("th[data-bind*='"+label+"']");
+                $(field).attr("data-bind", replace);
+            }
+            else
+            {
+                this.items.sort(sortASC);
+                var label = _gridField.replace("Name","");
+                replace = $(event.currentTarget).attr("data-bind");
+                replace = replace.replace("ASC", "DESC");
+                var field = $("th[data-bind*='"+label+"']");
+                $(field).attr("data-bind", replace);
+            }
+
+
+
+
+            /*function (a, b) {
+                if(this.sortOrder == "ASC")
+                    return a[this.gridField ] > b[this.gridField] ? -1 : 1;
+                else
+                    return a[this.gridField ] < b[this.gridField] ? -1 : 1;
+            });*/
+        }
     };
 
     ko.applyBindings(myModel);
+
+
+
+    function sortASC(a,b)
+    {
+        return a[gridField] < b[gridField] ? -1 : 1;
+    }
+
+    function sortDESC(a,b)
+    {
+        return a[gridField] > b[gridField] ? -1 : 1;
+    }
+
 });
